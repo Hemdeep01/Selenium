15ca9d108a34c183aa93e4cf6a25cceb8fcac8ec
Hem <you@example.com>
2020-04-25 20:22:37 -0400

    Action class

---
+++ Selenium/src/pckge/ActionsClassDemo.java
@@ -0,0 +1,25 @@
+package pckge;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class ActionsClassDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Hem\\Desktop\\SELENIUM\\Selenium Again\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.com/");
+		WebElement move= driver.findElement(By.id("nav-link-accountList"));
+		Actions a= new Actions(driver);
+		a.moveToElement(move).build().perform();
+		a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello").build().perform();
+		a.moveToElement(move).contextClick().build().perform();
+	}
+
+}
